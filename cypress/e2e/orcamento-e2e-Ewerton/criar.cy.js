bf906940dff65577eadab354140efe46591b0882
Ewerton Rafael <ton_rafael@hotmail.com>
2025-07-18 17:08:10 -0300

    refactor: ajustes na nomenclatura de arquivos e pastas relacionados a orçamento.

---
+++ cypress/e2e/orcamento-e2e-Ewerton/criar.cy.js
@@ -0,0 +1,26 @@
+import criarOrcamento from '../../pages/orcamento-pages-Ewerton/criar'
+
+// Ignora erros da aplicação causados por OneSignal ou iframes
+Cypress.on('uncaught:exception', (err) => {
+    return err.message.includes('postMessage') ? false : true
+})
+
+beforeEach(function () {
+    cy.fixture('usuario').then((usuario) => {
+        cy.login(usuario)
+    })
+    cy.fixture('orcamento').as('dadosOrcamento')
+})
+
+describe('Criar Orçamento', () => {
+    it('Deve criar um orçamento com sucesso', function () {
+        criarOrcamento.acessarPaginaOrcamento()
+        criarOrcamento.botaoNovoOrcamento()
+        criarOrcamento.dadosDoCliente(this.dadosOrcamento)
+        criarOrcamento.proximo()
+        criarOrcamento.dadosDoServico(this.dadosOrcamento)
+        criarOrcamento.botaoEnviar()
+        criarOrcamento.validarMensagemSucesso()
+        criarOrcamento.finalizarOrcamento()
+    })
+})
