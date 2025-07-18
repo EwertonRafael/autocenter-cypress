c3bd35b5b6ed0c61a1fbe67c76e48d9be207b072
Ewerton Rafael <ton_rafael@hotmail.com>
2025-07-17 23:49:48 -0300

    Task-Orcamento: implementando o caso de teste visualização do orçamento

---
+++ cypress/pages/visualizarOrcamento-Ewerton/elements.js
@@ -0,0 +1,5 @@
+export const elements = {
+    linhaSelecionada: '//*[@id="root"]/div[1]/main/section/div/section/div/div[2]/div/table/tr[7]/td[1]/div/div',
+    botaoVisualizar: '//*[@id="root"]/div[1]/main/section/div/section/div/div[2]/div/table/div/div/div/button[2]',
+    visualizacaoDoOrcamento: '.container_content__5EWFF'
+};
